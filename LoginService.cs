837ecea2820ab6220b3583f29d61c077aefa5fdf
raviklog <reach_ravik@hotmail.com>
2026-06-25 15:33:37 +0530

    Login Service changes for deliberate error

--- LoginService.cs
+++ LoginService.cs
@@ -6,7 +6,7 @@
         {
             try
             {
-                int x = 5 / 0; // deliberate error
+                int x = 6 / 0; // deliberate error
             }
             catch (Exception)
             {
